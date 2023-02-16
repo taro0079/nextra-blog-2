542a2d34b3d6002aefadf83a90c5cae702ea3004
Taro Morita <morita0079@gmail.com>
2023-02-16 18:41:39 +0900

    markdown にスタイル対応 (prose)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Headers from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Headers />
+      <div className="prose flex px-8 mx-auto my-4 justify-start">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
