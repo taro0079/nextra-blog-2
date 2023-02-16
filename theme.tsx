5c5eaae7f6636ed03bd2b1607d20c1d9e4bd8c34
Taro Morita <morita0079@gmail.com>
2023-02-16 21:15:23 +0900

    MDXProviderはいらなかった

--- theme.tsx
+++ theme.tsx
@@ -1,16 +1,8 @@
 import type { NextraThemeLayoutProps } from "nextra";
-import { MDXProvider, Components } from "nextra/mdx";
-import Footer from "./components/Footer";
 
 const components = {};
 export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
-  const { route } = pageOpts;
-  if (route.startsWith("/posts")) {
-    return (
-      <div className="prose">
-        <MDXProvider components={components}>{children}</MDXProvider>
-      </div>
-    );
-  }
+  const { pageMap } = pageOpts;
+  console.log(pageMap);
   return <div>{children}</div>;
 }
