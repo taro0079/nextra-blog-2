32a49d41577ce620f40944923f5af55f86244343
Taro Morita <morita0079@gmail.com>
2023-02-16 21:15:53 +0900

    記事を並べる

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,17 @@
-const Post = () => {
-  return <div>test</div>;
+import Link from "next/link";
+import { NextraThemeLayoutProps } from "nextra";
+import posts from "post.json";
+const Post = ({ pageOpts }: NextraThemeLayoutProps) => {
+  return (
+    <div>
+      <div className="text-4xl">Posts</div>
+      {posts.posts.map((post) => (
+        <Link href={post.slug} key={post.slug}>
+          <div>{post.title}</div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Post;
